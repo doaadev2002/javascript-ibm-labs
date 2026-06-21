9f76dcaa9dc580ada2b2a27008e531d2f6c6ee6e
Your Name <you@example.com>
2026-06-21 10:18:14 -0400

    Complete grocery tracker practice task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,12 +1,9 @@
-let length;
-let width;
+function groceryTracker() {
+    let amount1 = parseFloat(document.getElementById('grocery1').value);
+    let amount2 = parseFloat(document.getElementById('grocery2').value);
+    let amount3 = parseFloat(document.getElementById('grocery3').value);
 
-function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    
-    width = parseFloat(document.getElementById('width').value);
+    let total = amount1 + amount2 + amount3;
 
-    let area = length * width;
-
-    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+    document.getElementById('result').innerText = `The total amount for the grocery purchase is: $${total}`;
 }
